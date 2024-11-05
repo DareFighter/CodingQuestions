4b64cf7b3151ddcde182ab9f6477a59530612dc4
shreyas200012 <kumarrshreyas97@gmail.com>
2024-11-05 22:42:48 +0530

    added better solution for KthDistance Problem

--- CodingQuestions/src/Arrays/KthDistance.java
+++ CodingQuestions/src/Arrays/KthDistance.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class KthDistance {
 
-    public static boolean checkDuplicatesWithinK(int[] arr, int k) {
+    public static boolean bruteForce(int[] arr, int k) {
         // your code
         boolean res = false;
 
@@ -24,4 +24,18 @@ public class KthDistance {
         return res;
     }
 
+    public static boolean checkDuplicatesWithinK(int[] arr, int k) {
+        // your code
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (hashSet.contains(arr[i])) {
+                return true;
+            }
+            hashSet.add(arr[i]);
+            if (i >= k) {
+                hashSet.remove(arr[i - k]);
+            }
+        }
+        return false;
+    }
 }
